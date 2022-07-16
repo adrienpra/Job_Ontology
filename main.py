095f26daf7f23a395485c0e669c6984479108a21
adrien <adrienpereirabury@gmail.com>
2022-07-16 16:25:47 +0200

    implem bl_iw

--- main.py
+++ main.py
@@ -12,43 +12,34 @@ import time
 #__inputs__
 titles = read_file("map_input_IT.txt")
 iteration = 1
-
-if len(titles) == 1:
-    filename = "links_" + titles[0] + ".txt"
-else:
-    filename = "links.txt"
+filename = "links.txt"
 
 #__main__
 
 if __name__=="__main__":
     tps_start = time.time()
 
+    #get inputs
     stopwords = read_file("stopwords.txt")
     infoboxes = read_file("infoboxes.txt")
     titles = sort_map_input(titles, stopwords, infoboxes)
     print(titles)
     
-    if len(titles) == 1:
-        data_bl, arc_bl, label_bl = map_input_bl(titles[0], iteration, stopwords, infoboxes)      #One search in links
-        data_iw, arc_iw, label_iw = map_input_iw(titles[0], iteration, stopwords, infoboxes)      #One search out links
+    #process inputs: wiki-api scrap
+    p = mp.Pool()
+    result_bl = p.map(partial(map_input_bl, iteration=iteration, stopwords=stopwords, infoboxes=infoboxes), titles)
+    p.close()
+    
+    q = mp.Pool()
+    result_iw = q.map(partial(map_input_iw, iteration=iteration, stopwords=stopwords, infoboxes=infoboxes), titles)
+    q.close()
+    
+    #merge result
+    result = result_bl + result_iw
+    data, arc, label = zip(*result)
+    data, arc, label = merge_map_input(data, arc, label)
 
-        data = [data_bl] + [data_iw]
-        arc = [arc_bl] + [arc_iw]
-        label = [label_bl] + [label_iw]
-        
-    else:
-        p = mp.Pool()
-        result_bl = p.map(partial(map_input_bl, iteration=iteration, stopwords=stopwords, infoboxes=infoboxes), titles)
-        p.close()
-        
-        q = mp.Pool()
-        result_iw = q.map(partial(map_input_iw, iteration=iteration, stopwords=stopwords, infoboxes=infoboxes), titles)
-        q.close()
- 
-        result = result_bl + result_iw
-        data, arc, label = zip(*result)
-        data, arc, label = merge_map_input(data, arc, label)
-
+    #write result
     writeData(data, arc, label, filename)
 
     tps_end = time.time()
